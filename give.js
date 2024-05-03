a9d605faa7a918443fcb656f7e75e73cf33c1042
TheMaddoxJ <maddoxjohnson7@gmail.com>
2024-05-03 08:06:27 -0400

    giving venmo and cashapp removed

--- give.js
+++ give.js
@@ -1,4 +1,5 @@
 // Function for the give online button
 function redirectToGivingLink() {
-    window.location.href = 'https://app.easytithe.com/app/giving/cheerful';
+    // window.location.href = 'https://app.easytithe.com/app/giving/cheerful';
+    window.location.href = 'https://rlclex.breezechms.com/give/online';
   }
